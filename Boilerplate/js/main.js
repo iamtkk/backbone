bd59f96f4b2d0f2c60f2ec91245acc518a223dde
김태관 <iamtk@neowiz.com>
2024-01-15 21:43:47 +0900

    2

--- Boilerplate/js/main.js
+++ Boilerplate/js/main.js
@@ -117,35 +117,62 @@
 // songView.render();
 // $("#container").html(songView.$el);
 
+// var Song = Backbone.Model.extend();
+
+// var Songs = Backbone.Collection.extend({
+//   model: Song,
+// });
+
+// var SongView = Backbone.View.extend({
+//   tagName: "li",
+//   render: function () {
+//     this.$el.html(this.model.get("title"));
+//     return this;
+//   },
+// });
+
+// var SongsView = Backbone.View.extend({
+//   render: function () {
+//     var self = this;
+//     this.model.each(function (song) {
+//       var songView = new SongView({ model: song });
+//       self.$el.append(songView.render().$el);
+//     });
+//   },
+// });
+
+// var songs = new Songs([
+//   new Song({ title: "Blue in Green" }),
+//   new Song({ title: "So What" }),
+//   new Song({ title: "All Blues" }),
+// ]);
+
+// var songsView = new SongsView({ el: "#container", model: songs });
+// songsView.render();
+
 var Song = Backbone.Model.extend();
 
-var Songs = Backbone.Collection.extend({
-  model: Song,
-});
-
 var SongView = Backbone.View.extend({
-  tagName: 'li',
+  events: {
+    click: "onClick",
+    "click .bookmark": "onClickBookmark",
+  },
+  onClick: function () {
+    console.log("Listen Clicked");
+  },
+  onClickBookmark: function (e) {
+    e.stopPropagation();
+    console.log("Bookmark Clicked");
+  },
   render: function () {
-    this.$el.html(this.model.get('title'));
+    this.$el.html(
+      this.model.get("title") +
+        " <button>Listen</button> <button class='bookmark'>Bookmark</button>"
+    );
     return this;
   },
 });
 
-var SongsView = Backbone.View.extend({
-  render: function () {
-    var self = this;
-    this.model.each(function (song) {
-      var songView = new SongView({ model: song });
-      self.$el.append(songView.render().$el);
-    });
-  },
-});
-
-var songs = new Songs([
-  new Song({ title: 'Blue in Green' }),
-  new Song({ title: 'So What' }),
-  new Song({ title: 'All Blues' }),
-]);
-
-var songsView = new SongsView({ el: '#container', model: songs });
-songsView.render();
+var song = new Song({ title: "Blue in Green" });
+var songView = new SongView({ el: "#container", model: song });
+songView.render();
